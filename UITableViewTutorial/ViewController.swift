ceff14c3132f774210e2824eb235dbd4c9aef5fa
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2020-06-03 01:06:43 -0500

    Initial Commit

---
+++ UITableViewTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewTutorial
+//
+//  Created by Evgeniy Poznyak on 6/3/20.
+//  Copyright © 2020 Evgeniy Poznyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
